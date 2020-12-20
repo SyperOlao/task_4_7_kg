67e19ac11a53d94d918392efa83eb71d0f573b4e
Syper_olao <olay1234538@gmail.com>
2020-12-20 23:33:04 +0300

    add Camera interface and scenna class

---
+++ task_4_7_kg/ICamera.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_4_7_kg
+{
+    interface ICamera
+    {
+        Vector3 w2c(Vector3 vector3); 
+    }
+}
---
+++ task_4_7_kg/Scena.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace task_4_7_kg
+{
+    class Scena
+    {
+        private List<IModel> models;// = new ArrayList<IModel>();
+        
+    }
+}
